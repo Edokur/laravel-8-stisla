0146fbf26ae04c416ed13c9f4cfa57884f84cc81
Edokur <edok7179@gmail.com>
2023-05-22 19:35:31 +0700

    asset stisla

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+@section('content')
+<section class="section">
+  <div class="section-header">
+    <h1>Dashboard</h1>
+</div>
+
+<div class="section-body">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores impedit fuga nam, atque magnam, molestiae aspernatur nesciunt, adipisci voluptatum quia velit nulla quis nobis sunt! Suscipit possimus ut libero provident quos alias harum architecto iste reprehenderit! Aliquam ullam quaerat, minus eum pariatur, mollitia accusantium nemo aut enim reiciendis tempora officiis eligendi perferendis praesentium numquam. Possimus recusandae quidem explicabo deserunt tempore veniam nesciunt eveniet excepturi minus vitae aspernatur adipisci at dolorem laboriosam laborum ducimus, voluptatum a sed perferendis. Dolorem assumenda fugiat ipsa excepturi laborum eaque ipsum minus, quo, mollitia earum voluptatibus?</p>
+  </div>
+</section>
+    
+@endsection
